0b1bb020750f4b30a34595e9050411e79c2c9db5
Aaron Know <aaronhdz1919@gmail.com>
2024-01-20 21:46:20 -0700

    The tables are done and made their realtions

--- tables.sql
+++ tables.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbPerson
 
 CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbPeriodicity
 (
-    id          INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    id          TINYINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
     PeriType    VARCHAR(30) NOT NULL UNIQUE,
     Descrip     VARCHAR(60),
     PRIMARY KEY (id)
@@ -56,6 +56,17 @@ CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbRealState
     PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbAutomovil
+(
+	id    			SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+	Make	 		VARCHAR(30) NOT NULL UNIQUE,
+    Model           VARCHAR(30) NULL,
+    Yearr			VARCHAR(4)  NULL,
+    Descrip         VARCHAR(30) NULL,
+        
+    PRIMARY KEY (id)
+)ENGINE = InnoDB;
+
 CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbDependent
 (
 	id    			SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
@@ -66,7 +77,7 @@ CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbDependent
     PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
--- TODO revisar esta tabla, aunque parece que si la termine
+
 CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbCreditCard
 (
 	id    			SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
@@ -79,9 +90,13 @@ CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbCreditCard
     PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
-
-
-
+CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbOutcomeType
+(
+    id          TINYINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    OutType     VARCHAR(30) NOT NULL UNIQUE,
+    Descrip     VARCHAR(60),
+    PRIMARY KEY (id)
+)ENGINE = InnoDB;
 
 
 -- //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -89,6 +104,64 @@ CREATE TABLE IF NOT EXISTS bdMoneyInOut.tbCreditCard
 -- // ******************* CREAING SON TABLES ******************************//
 
 -- //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+CREATE TABLE IF NOT EXISTS bdmoneyinout.tbUser
+(
+	id    			SMALLINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+	UserName  		VARCHAR(40) UNIQUE NOT NULL,
+    passwrd         VARCHAR(100) NOT NULL,
+    
+    idPerson        SMALLINT UNSIGNED,
+    PRIMARY KEY (id),
+    KEY idx_fk_idPerson(idPerson),    
+    CONSTRAINT fk_User_Person FOREIGN KEY (idPerson) REFERENCES tbPerson (id) ON DELETE CASCADE 
+)ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS bdmoneyinout.tbMovement
+(
+	id    			INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+	Amount 		    DECIMAL(10,2) DEFAULT 0 COMMENT 'Income or Outcome quantity',
+	Balance		    DECIMAL(10,2) DEFAULT 0 COMMENT 'Money after store movement',
+    DateMv          DATETIME NOT NULL,
+    Descrip         VARCHAR(60) NOT NULL,
+    
+    idUser          SMALLINT UNSIGNED,
+    idCatalogue     SMALLINT UNSIGNED,
+    idGroup			SMALLINT UNSIGNED,
+    idPeriodicity	TINYINT  UNSIGNED,
+    
+    PRIMARY KEY (id),
+    INDEX idx_fk_idUser(idUser),    
+    CONSTRAINT fk_Movement_User FOREIGN KEY (idUser) REFERENCES tbUser (id) ON DELETE NO ACTION,
+       
+    CONSTRAINT fk_Movement_TypeCatalogue FOREIGN KEY (idCatalogue) REFERENCES tbtypecatalogue (id) ON DELETE NO ACTION ,    
+    CONSTRAINT fk_Movement_GroupInOut FOREIGN KEY (idGroup) REFERENCES tbgroupinout (id) ON DELETE NO ACTION,
+    CONSTRAINT fk_Movement_Periodicity FOREIGN KEY (idPeriodicity) REFERENCES tbperiodicity (id) ON DELETE NO ACTION
+)ENGINE = InnoDB;
+
+-- todo: terminar esta tabla
+CREATE TABLE IF NOT EXISTS bdmoneyinout.tbOutcomeDetail
+(
+	id    			INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+	
+    idTypeOut	 	TINYINT UNSIGNED NOT NULL,
+    idOutTable      SMALLINT UNSIGNED NOT NULL, -- TO STORE ID FROM 4 TABLES OF OUTCOMES.
+    idMovement 		INT UNSIGNED NOT NULL,   
+    
+    PRIMARY KEY (id),
+    CONSTRAINT fk_OutcomeDetail_TypeOut FOREIGN KEY (idTypeOut) REFERENCES tboutcometype (id) ON DELETE NO ACTION,
+    INDEX idx_fk_idTypeOut(idTypeOut),    
+    CONSTRAINT fk_OutcomeDetail_Movement FOREIGN KEY (idMovement) REFERENCES tbmovement (id) ON DELETE NO ACTION,
+    
+    CONSTRAINT fk_OutcomeDetail_RealState  FOREIGN KEY (idOutTable) REFERENCES tbrealstate  (id) ON DELETE NO ACTION,
+    CONSTRAINT fk_OutcomeDetail_Automovil  FOREIGN KEY (idOutTable) REFERENCES tbautomovil  (id) ON DELETE NO ACTION,
+    CONSTRAINT fk_OutcomeDetail_Dependent  FOREIGN KEY (idOutTable) REFERENCES tbdependent  (id) ON DELETE NO ACTION,
+    CONSTRAINT fk_OutcomeDetail_CreditCard FOREIGN KEY (idOutTable) REFERENCES tbcreditcard (id) ON DELETE NO ACTION    
+    
+)ENGINE = InnoDB;
+
+-- TODO:HACER LA TABLA PARA REGISTRAR TODOS LOS MOVIMIENTOS DE ENTRADA Y SALIDA
+
 
 CREATE TABLE IF NOT EXISTS bdmoneyinout.tbInputType
 (
@@ -115,17 +188,7 @@ CREATE TABLE IF NOT EXISTS bdmoneyinout.tbExitType
 )ENGINE = InnoDB;
 
 
-CREATE TABLE IF NOT EXISTS bdmoneyinout.tbUser
-(
-	id    			INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-	UserName  		VARCHAR(40) UNIQUE NOT NULL,
-    passwrd         VARCHAR(100) NOT NULL,
-    
-    idPerson        INT UNSIGNED,
-    PRIMARY KEY (id),
-    KEY idx_fk_idPerson(idPerson),    
-    CONSTRAINT fk_User_Person FOREIGN KEY (idPerson) REFERENCES tbPerson (id) ON DELETE CASCADE 
-)ENGINE = InnoDB;
+
 
 CREATE TABLE IF NOT EXISTS bdmoneyinout.tbMoneyBalnce
 (
